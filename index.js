cb4efa078422501c8ad77733956d7f3b73b5fa29
rootegg <344163452@qq.com>
2022-09-28 11:37:36 +0800

    init

--- index.js
+++ index.js
@@ -2,19 +2,31 @@
 
 const commander = require("commander");
 const chalk = require("chalk");
-const ora = require('ora')
+const ora = require("ora");
 const downgit = require("download-git-repo");
 const pkg = require("./package.json");
+const fs = require("fs");
+
 // 定义命令 vbcli create
-commander.command("create").action(function(){
+commander.command("create").action(function () {
+  const existFiles = fs.readdirSync("./");
+  if (existFiles.length) {
+    return console.log(chalk.red("失败，请在空目录下执行命令"));
+  }
+
   const spinner = ora("下载中").start();
-  downgit("github:rootegg/vbtemplate", process.cwd(), { clone: false }, function (err) {
-    spinner.stop();
-    if (err) {
-      throw err;
+  downgit(
+    "github:rootegg/vbtemplate",
+    process.cwd(),
+    { clone: false },
+    function (err) {
+      spinner.stop();
+      if (err) {
+        throw err;
+      }
+      console.log(chalk.green("下载成功"));
     }
-    console.log(chalk.green("下载成功"));
-  });
+  );
 });
 // 定义版本
 commander.version(pkg.version);
